bcaaddbae933f2df0e2f5709ad558b5673e85287
Franco Ortiz <ortizfranco48@yahoo.com>
2024-03-05 18:35:51 -0300

    react hook form

--- client/src/elements/TaskFormPage.jsx
+++ client/src/elements/TaskFormPage.jsx
@@ -1,56 +1,46 @@
 import React, { useState } from "react";
+import {useForm} from "react-hook-form"
 
 function TaskFormPage() {
-  const [formData, setFormData] = useState({
-    title: "",
-    description: "",
-  });
+  const{register, handleSubmit} = useForm()
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData((prevState) => ({
-      ...prevState,
-      [name]: value,
-    }));
-  };
+  const onSubmit = handleSubmit((data)=> {
+    console.log(data)
+  })
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    const formData = new FormData(e.target);
+  // const handleSubmit = async (e) => {
+  //   e.preventDefault();
+  //   const formData = new FormData(e.target);
 
-    const response = await fetch("http://localhost:8080/api/tasks/create", {
-      method: "POST",
-      body: formData,
-    });
-    if (response.ok) {
-      const data = await response.json();
-      console.log(data)
-      // Redirect to the specified URL
-      window.location.href = `/tasks/${data._id}`;
-    } else {
-      // Handle error response
-      const errorData = await response.json();
-      setErrorMessage(errorData.message);
-    }
-  };
+  //   const response = await fetch("http://localhost:8080/api/tasks/create", {
+  //     method: "POST",
+  //     body: formData,
+  //   });
+  //   if (response.ok) {
+  //     const data = await response.json();
+  //     console.log(data)
+  //     // Redirect to the specified URL
+  //     window.location.href = `/tasks/${data._id}`;
+  //   } else {
+  //     // Handle error response
+  //     const errorData = await response.json();
+  //     setErrorMessage(errorData.message);
+  //   }
+  // };
 
   return (
     <div>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={onSubmit}>
         <input
           type="text"
-          name="title"
-          id="title"
           placeholder="Title"
           autoFocus
-          onChange={handleChange}
+          {...register("title")}
         />
         <input
           type="text"
-          name="description"
-          id="description"
+          {...register("description")}
           placeholder="Description"
-          onChange={handleChange}
         />
         <button>Save</button>
       </form>
